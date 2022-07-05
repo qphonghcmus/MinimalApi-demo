b29659e1e06644c07981b6a5e3214dc9ceec2ba4
Quoc Phong <qphongqh@gmail.com.vn>
2022-07-05 16:42:18 +0700

    first commit

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿If not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Cristiano', 'Ronaldo'),
+	('Lionel', 'Messi'),
+	('Ricardo', 'Kaka'),
+	('Fernando', 'Torres');
+end
